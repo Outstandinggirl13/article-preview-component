88dfd14749637f71ab71d0b459740be2ad43d1e9
Outstandinggirl13 <108367073+Outstandinggirl13@users.noreply.github.com>
2024-10-19 23:10:33 +0300

    Removed more redundant code.

--- app.js
+++ app.js
@@ -6,25 +6,35 @@ const previewWindow = document.querySelector(".article-preview__window");
 
 const windowButton = document.querySelector(".article-preview__window-button");
 
-contentBlockButton.addEventListener("mouseenter", function(e) {
-    this.style.cursor = "pointer";
-    this.style.backgroundColor = "#6d7f97";
-    this.firstElementChild.firstElementChild.style.fill = "#ecf2f8";
-});
+function invertColors(x) {
+    x.style.backgroundColor = "#6d7f97";
+    x.firstElementChild.firstElementChild.style.fill = "#ecf2f8";
+}
 
-contentBlockButton.addEventListener("mouseleave", function(e) {
-    this.style.backgroundColor = "#ecf2f8";
-    this.firstElementChild.firstElementChild.style.fill = "#6d7f97";
+function restoreColors(x) {
+    x.style.backgroundColor = "#ecf2f8";
+    x.firstElementChild.firstElementChild.style.fill = "#6d7f97";
+}
 
-});
-
-contentBlockButton.addEventListener("click", function(e) {
+function toggleDisplay() {
     if (previewWindow.style.display === 'flex') {
         previewWindow.style.display = 'none';
     } else {
         previewWindow.style.display = 'flex';
     }
+}
 
+contentBlockButton.addEventListener("mouseenter", function(e) {
+    this.style.cursor = "pointer";
+    invertColors(this);
+});
+
+contentBlockButton.addEventListener("mouseleave", function(e) {
+    restoreColors(this);
+});
+
+contentBlockButton.addEventListener("click", function(e) {
+    toggleDisplay();
     if (window.innerWidth < 778) {
         contentBlock.style.paddingBottom = '0px';
     }
@@ -32,20 +42,14 @@ contentBlockButton.addEventListener("click", function(e) {
 
 windowButton.addEventListener("mouseenter", function(e) {
     e.target.style.cursor = "pointer";
-    this.style.backgroundColor = "#ecf2f8";
-    this.firstElementChild.firstElementChild.style.fill = "#6d7f97";
+    restoreColors(this);
 });
 
 windowButton.addEventListener("mouseleave", function(e) {
-    this.style.backgroundColor = "#6d7f97";
-    this.firstElementChild.firstElementChild.style.fill = "#ecf2f8";
+    invertColors(this);
  });
 
 windowButton.addEventListener("click", function(e) {
-        if (previewWindow.style.display === 'flex') {
-            previewWindow.style.display = 'none';
-        } else {
-            previewWindow.style.display = 'flex';
-        }
-        contentBlock.style.paddingBottom = '20px';
+    toggleDisplay();
+    contentBlock.style.paddingBottom = '20px';
 });
